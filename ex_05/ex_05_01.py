b4ce287628a8b6c681dc2be2ef3cb047489c3c9d
Nate <bassmannate@users.noreply.github.com>
2021-09-22 01:45:06 +0000

    updated 5-1 and created .replit

--- ex_05/ex_05_01.py
+++ ex_05/ex_05_01.py
@@ -1,12 +1,14 @@
-pieces = list()
+count = 0
+total = 0
 while True:
   num = input("Please enter a number: ")
   if num == "done":
     break
   try:
-    num = int(num)
+    num = float(num)
   except:
     print("Input must be numeric")
-    exit()
-  pieces = pieces.append(num)
-print(pieces)
+    continue
+  total = total + num
+  count = count +1
+print(total,count,total / count)
