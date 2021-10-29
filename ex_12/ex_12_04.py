413db62c37f5cf1c441fbae81c8d602b2656ba74
Nate <bassmannate@users.noreply.github.com>
2021-10-29 17:27:27 +0000

    Finished 12_04!

---
+++ ex_12/ex_12_04.py
@@ -0,0 +1,26 @@
+# To run this, download the BeautifulSoup zip file
+# http://www.py4e.com/code3/bs4.zip
+# and unzip it in the same directory as this file
+
+import urllib.parse, urllib.error
+from urllib.request import Request, urlopen
+from bs4 import BeautifulSoup
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+count = 0
+url = input('Enter - ')
+req = Request(url, headers = {'User-Agent':'Mozilla/5.0'})
+html = urlopen(req).read()
+soup = BeautifulSoup(html, 'html.parser')
+
+# Retrieve all of the p tags
+tags = soup('p')
+for tag in tags:
+  count += 1
+
+print("Count: ",count)
